9aed27be36bd16723e2e4f99805c900bde7cff95
rua maruoka <rua.sun218@gmail.com>
2022-06-20 23:43:22 +0900

    画像を投稿できるようにした

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -16,17 +16,14 @@ class PicturesController < ApplicationController
   end
 
   def create
-    # @picture = Picture.new(picture_params)
-    # @picture.user_id = current_user.id
     @picture = current_user.pictures.build(picture_params)
-
-    respond_to do |format|
+    if params[:back]
+      render :new
+    else
       if @picture.save
-        format.html { redirect_to picture_url(@picture), notice: "Picture was successfully created." }
-        format.json { render :show, status: :created, location: @picture }
+        redirect_to pictures_path, notice: "画像を投稿しました！"
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
+        render :new
       end
     end
   end
