a84cb2f9e34d29709751a8f3b354dc3468a76280
rua maruoka <rua.sun218@gmail.com>
2022-06-21 10:53:06 +0900

    名前、メールなどを日本語にした

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  root 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
-  resources :pictures
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
 end
