abe7bfd71e2b21b487751c75e6a2e39655699a40
rua maruoka <rua.sun218@gmail.com>
2022-06-22 11:10:32 +0900

    userのプロフィール画像が登録できるようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :image, :image_cache)
   end
 
 end
