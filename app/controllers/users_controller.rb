bc0a6309de184177f9ae9da1301cc909904e69ad
rua maruoka <rua.sun218@gmail.com>
2022-06-24 15:58:29 +0900

    修正エラー

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
   before_action :ensure_current_user, {only: [:edit, :update]}
+  
   def ensure_current_user
     if current_user.id != params[:id].to_i
       flash[:notice]="権限がありません"
@@ -18,7 +20,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id)
+      redirect_to new_session_path
     else
       render :new
     end
